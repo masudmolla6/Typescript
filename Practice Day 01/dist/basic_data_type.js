2987a0da7de1eb9025ad70d23c981255084d3aa2
masudmolla6 <masudmolla2937@gmail.com>
2026-06-26 21:07:25 +0600

    basic data typr

---
+++ Practice Day 01/dist/basic_data_type.js
@@ -0,0 +1,7 @@
+// String
+const favouritGame = "Football";
+// console.log(favouritGame);
+// NUmber
+let math_mark = 34;
+console.log(math_mark);
+export {};
---
+++ Practice Day 01/src/basic_data_type.ts
@@ -0,0 +1,27 @@
+// String
+const favouritGame:string="Football";
+// console.log(favouritGame);
+
+// NUmber
+let math_mark : number =34;
+// console.log(math_mark);
+
+// Boolean
+let isAdmin:boolean=true;
+
+// null
+let x : null=null;
+
+
+// Array
+
+let friens : string[]=["Masud","Karim"];
+
+let studentsIdNumbers:number[]=[2,3,4,5,6,7];
+
+// tuple -> special type of array->order -> type of value
+
+let coordinates:[number, number]=[4,5];
+
+let nameAge:[string, number, boolean]=["Masud", 28, true];
+
