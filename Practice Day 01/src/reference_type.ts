3cc2131151d6617e5e05a4d16d5ddf43cce8b4ad
masudmolla6 <masudmolla2937@gmail.com>
2026-06-26 22:11:25 +0600

    referencetype

---
+++ Practice Day 01/src/reference_type.ts
@@ -0,0 +1,14 @@
+// Rederence Type -> Object
+
+const user:{
+    company:"Programming hero"; //literal type
+    firstName:string;
+    middleName?:string; //optional type
+    lastName:string;
+    isMarried:boolean;
+}={
+    company:"Programming hero",
+    firstName:"Masud",
+    lastName:"Rana",
+    isMarried:true,
+}
