e16a177091e7a9d8a4a9b54f204b4354838a6d1c
masudmolla6 <masudmolla2937@gmail.com>
2026-07-02 22:40:30 +0600

    asynchgronous typescript

---
+++ Practice Day 02/asynchronous_typescript.ts
@@ -0,0 +1,57 @@
+                  // ---string---
+
+// const createPromise=():Promise<string>=>{
+//     return new Promise<string>((resolve, reject)=>{
+//         const data:string="Something";
+//         if(data)
+//             resolve(data)
+//         else
+//             reject("Failed To Load Data");
+//     })
+// }
+                  // ---Number---
+
+// const createPromise=():Promise<number>=>{
+//     return new Promise<number>((resolve, reject)=>{
+//         const data:number=3455;
+//         if(data)
+//             resolve(data)
+//         else
+//             reject("Failed To Load Data");
+//     })
+// }
+
+                  // ---boolean---
+
+const createPromise=():Promise<boolean>=>{
+    return new Promise<boolean>((resolve, reject)=>{
+        const data:boolean=true;
+        if(data)
+            resolve(data)
+        else
+            reject("Failed To Load Data");
+    })
+}
+
+
+
+// const showData=async():Promise<boolean>=>{
+//     const data:boolean=await createPromise();
+//     return data;
+// }
+
+// const mainFun=async()=>{
+//     const result=await showData();
+//     console.log(result);
+// }
+
+// mainFun();
+
+
+const getToDo=async()=>{
+    const response=await fetch('https://jsonplaceholder.typicode.com/todos/1');
+    const data=await response.json();
+    console.log(data);
+}
+
+getToDo();
